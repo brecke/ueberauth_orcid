ccf6ddbef2dd2649734578d0a4f3efda7865aed4
Miguel Laginha <me@miguellaginha.com>
2023-06-22 15:07:46 +0100

    build: new deps

--- mix.exs
+++ mix.exs
@@ -1,14 +1,33 @@
 defmodule UeberauthOrcid.MixProject do
   use Mix.Project
 
+  @source_url "https://github.com/brecke/ueberauth_orcid"
+  @version "0.2.2"
+
   def project do
     [
       app: :ueberauth_orcid,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
+      docs: docs(),
       package: package()
+    ]
+  end
+
+  defp docs do
+    [
+      extras: [
+        "CHANGELOG.md": [title: "Changelog"],
+        "CONTRIBUTING.md": [title: "Contributing"],
+        LICENSE: [title: "License"],
+        "README.md": [title: "Overview"]
+      ],
+      main: "readme",
+      source_url: @source_url,
+      source_ref: "#v{@version}",
+      formatters: ["html"]
     ]
   end
 
@@ -24,18 +43,23 @@ defmodule UeberauthOrcid.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-      {:oauth2, "~> 1.0 or ~> 2.0"},
-      {:ueberauth, "~> 0.7"}
+      {:oauth2, "~> 2.0"},
+      {:ueberauth, "~> 0.10"},
+      {:credo, "~> 0.8", only: [:dev, :test], runtime: false},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
   end
 
   defp package do
     [
-      description: "An Ueberauth strategy for using Orcid to authenticate your users.",
+      description: "An Ueberauth strategy for using Orcid to authenticate your users via OAuth2.",
       files: ["lib", "mix.exs", "README.md", "LICENSE"],
       maintainers: ["Miguel Laginha"],
       licenses: ["MIT"],
+      links: %{
+        Changelog: "https://hexdocs.pm/ueberauth_orcid/changelog.html",
+        GitHub: @source_url
+      }
     ]
   end
-
 end
